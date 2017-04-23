bf4b2f31403f45743d9814077f2f4c096c2fa080
Tyron Wytrykowski <tyronwyt@gmail.com>
2017-04-23 16:39:11 +0100

    Refactor js to OO

--- assets/js/memory.js
+++ assets/js/memory.js
@@ -1,79 +1,127 @@
-var colours = ["red", "lime", "blue", "yellow"];
-var level = 5;
-var speed = 600;
-var colourOrder = [];
-var count = 0;
-var blocks;
-var myApp = {};
+var app = {
+
+// Declare vars
+  blockColours: ["red", "lime", "blue", "yellow"],
+  level: 5,
+  speed: 600,
+  colourOrder: [],
+  count: 0,
+  blocks: {},
+
+  init: function() {
+    this.cacheDom();
+    this.createBoard();
+    // this.bindEvents();
+  },
+
+// Cache DOM elements that will be reused
+  cacheDom: function() {
+    var game_board = document.getElementById('game_board');
+    var outputLevel = document.getElementById('level');
+  },
+
+// Create event listener
+  createEvent: function(name, id, func){
+    var name = document.getElementById(id);
+    name.onclick = function(){
+        func();
+    };
+
+  },
 
 
-function getColours() {
-    for (var i = 0; i < level; i++) {
-        var random =  Math.floor(Math.random() * (4 - 0)) + 0;
-        colourOrder.push(random);
-        console.log(i + " is " + colours[random]);
+  createBoard: function() {
+    //Create coloured squares
+    for (var i = 0; i < this.blockColours.length; i++) {
+      var block = document.createElement('div');
+      block.classList.add('block');
+      block.setAttribute("style", "background: " + this.blockColours[i]);
+      block.setAttribute("data-id", i);
+      game_board.appendChild(block);
     }
-}
+    // Create start button
+    var startButton = document.createElement('button');
+    startButton.innerHTML = "Start Game";
+    startButton.setAttribute("id", 'start_btn');
+    game_board.appendChild(startButton);
+    // this.createEvent("startBtn", "start_btn", this.getColours);
+    var startBtn = document.getElementById('start_btn');
+    startBtn.onclick = function(){
 
-function gameSetup() {
-  getColours();
-  var game_board = document.getElementById('game_board');
-  var outputLevel = document.getElementById('level');
-  outputLevel.textContent = "Level: " + level;
-  for (var i = 0; i < colours.length; i++) {
-    var block = document.createElement('div');
-    var bgColour = "background: " + colours[i];
-    block.classList.add('block');
-    block.setAttribute("style", bgColour);
-    block.setAttribute("data-id", i);
-    game_board.appendChild(block);
-  }
-  var startButton = document.createElement('button');
-  startButton.innerHTML = "Start Game";
-  startButton.setAttribute("id", 'start_btn');
-  game_board.appendChild(startButton);
-  document.getElementById('start_btn').onclick = function() {
-    startGame();
-  };
-}
+      app.startGame();
+    };
 
-function startGame() {
-  var blocks = document.getElementsByClassName('block');
+  },
 
-  delayLoop(level);
-}
 
-function delayLoop(to, at) {
-  var blocks = document.getElementsByClassName('block');
-  var at = at || 1;
-      if (at <= to) {
-        var selectedBlock = colourOrder[at-1];
-        setTimeout(function() {
-          blocks[selectedBlock].classList.add("blockOn");
-          fadeOut(blocks[selectedBlock], to, at)
-        }, 200);
+// create a random selection of colours based on level
+  getColours: function() {
+    this.colourOrder = [];
+    for (var i = 0; i < this.level; i++) {
+        var random =  Math.floor(Math.random() * (4 - 0)) + 0;
+        this.colourOrder.push(random);
+        console.log(i + " is " + this.blockColours[random]);
+    };
+  },
 
-      } else {
-        for (var i = 0; i < blocks.length; i++) {
-          blocks[i].onclick = function(){
-          var blockId = this.getAttribute("data-id");
-          console.log(blockId + " " + colourOrder[count]);
-            if (blockId == colourOrder[count]) {
-              console.log("correct");
-              count ++;
-            } else {
-              console.log("wrong");
-            }
-          };
+// start the game
+  startGame: function() {
+    app.getColours();
+    this.delayLoop(this.level);
+  },
+
+// Delay loop
+  delayLoop: function(to, at) {
+    var blocks = document.getElementsByClassName('block');
+    var at = at || 1;
+        if (at <= to) {
+          var selectedBlock = this.colourOrder[at-1];
+          setTimeout(function() {
+            blocks[selectedBlock].classList.add("blockOn");
+            app.fadeOut(blocks[selectedBlock], to, at);
+          }, 200);
+
+        } else {
+          for (var i = 0; i < blocks.length; i++) {
+            blocks[i].onclick = function(){
+              var blockId = this.getAttribute("data-id");
+
+              console.log(blockId + " " + app.colourOrder[app.count]);
+                if (blockId == app.colourOrder[app.count]) {
+                  console.log("correct");
+                  app.count ++;
+                  if (app.count >= level) {
+                    document.getElementById('response').innerHTML = "Correct";
+                    //place new button to restart with new level
+
+                  }
+                } else {
+                  console.log("wrong");
+                  for (var i = 0; i < blocks.length; i++) {
+                    blocks[i].setAttribute("style", "background: grey")
+                  }
+                  document.getElementById('response').innerHTML = "Wrong, try again";
+                }
+              };
+          }
         }
-      }
-  }
+  },
 
-  function fadeOut(block, to, at) {
+  fadeOut: function(block, to, at) {
     setTimeout(function() {
       block.classList.remove("blockOn");
-      delayLoop(to, at + 1);
+      app.delayLoop(to, at + 1);
     }, 500);
   }
 
-document.onload = gameSetup();
+
+
+};
+
+document.onload = app.init();
+
+
+
+//
+//
+// document.onload = gameSetup();
